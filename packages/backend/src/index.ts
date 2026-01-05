e5b205da23a7772b994d67b7de3d0e562c855082
Nathanael Liechti <technat@technat.ch>
2026-01-05 10:55:56 +0100

    feat: add github catalog backend plugin

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -85,6 +85,8 @@ backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 
